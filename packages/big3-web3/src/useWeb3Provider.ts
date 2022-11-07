453e1bcb285eed904a22ee853770b2cebf7ed9dc
Benhaben <web3monkey@proton.me>
2022-11-07 16:38:51 +0800

    Incorrect defination of readyState

--- packages/big3-web3/src/useWeb3Provider.ts
+++ packages/big3-web3/src/useWeb3Provider.ts
@@ -6,33 +6,21 @@ import { useWeb3React } from '.'
  * Provides a web3 provider with or without user's signer
  * Recreate web3 instance only if the provider change
  */
-const useWeb3Provider = (
-  simpleRpcProvider?: any
-): {
-  provider: ethers.providers.JsonRpcProvider | ethers.providers.Web3Provider
-  readyState: boolean
-} => {
+type Provider = ethers.providers.JsonRpcProvider | ethers.providers.Web3Provider
+const useWeb3Provider = (simpleRpcProvider?: Provider): { provider: Provider } => {
   const { library } = useWeb3React()
   const refEth = useRef(library)
   const [provider, setProvider] = useState(library || simpleRpcProvider)
-  const [readyState, setReadyState] = useState(false)
   const { account } = useWeb3React()
-
-  useEffect(() => {
-    if (!account) {
-      setReadyState(false)
-    }
-  }, [account])
 
   useEffect(() => {
     if (library !== refEth.current && account) {
       setProvider(library || simpleRpcProvider)
       refEth.current = library
-      setReadyState(true)
     }
   }, [library, account])
 
-  return { provider, readyState }
+  return { provider }
 }
 
 export default useWeb3Provider
