d39bc847780321c2cc0d39f5f1d2656c38d3699d
Benhaben <web3monkey@proton.me>
2023-03-02 12:35:33 +0800

    feat: big3-styled-base#Big3PortalNode

--- packages/big3-styled-base/src/components/Big3PortalNode/index.tsx
+++ packages/big3-styled-base/src/components/Big3PortalNode/index.tsx
@@ -1,19 +1,38 @@
 import { Big3ExpandProps, Big3Props } from 'interface'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FC } from 'react'
 import ReactDOM from 'react-dom'
 import { Big3Box } from '../../base'
 
-interface Big3PortalNodeProps extends Big3Props<HTMLDivElement> {
-  container?: HTMLElement
+interface Big3PortalNodeProps {
+  container?: HTMLElement | Function
 }
 
-const Big3PortalNode: FC<Big3PortalNodeProps> = props => {
+const Big3PortalNode: FC<Big3PortalNodeProps & Big3Props<HTMLDivElement>> = props => {
   const { children, container, ...rest } = props
-  return ReactDOM.createPortal(
-    <Big3Box {...(rest as Big3ExpandProps<HTMLDivElement>)}>{children}</Big3Box>,
-    container || document.body
-  )
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    ;(function checkIfMounted() {
+      if (typeof container === 'function') {
+        const dom = container()
+        if (!!dom) {
+          setMounted(true)
+        } else {
+          setTimeout(checkIfMounted, 200)
+        }
+      } else {
+        setMounted(!!container)
+      }
+    })()
+  }, [container])
+
+  return mounted
+    ? ReactDOM.createPortal(
+        <Big3Box {...(rest as Big3ExpandProps<HTMLDivElement>)}>{children}</Big3Box>,
+        typeof container === 'function' ? container() : container || document.body
+      )
+    : null
 }
 
 Big3PortalNode.displayName = Big3PortalNode.name
